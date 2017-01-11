5a14821e614735aaa6e24b57182f606e28705de1
Jose Camacho <josecamacho@students.ptechnyc.org>
2017-01-11 14:53:20 +0000

    made a house

---
+++ script.js
@@ -0,0 +1,14 @@
+   TinyTurtle.apply(window); 
+   
+   right(90);
+   forward(30);
+   right(90);
+   forward(30);
+   right(90);
+   forward(30);
+   right(90);
+   forward(30);
+   right(45);
+  forward(20);
+  right(90);
+  forward(22);
